6bf288c70bbbf8a19e7317c9b25222e5359b24b6
wxy4869 <wangxinyuan4869@163.com>
2022-08-02 14:41:27 +0800

    feat: 完成了CURD

--- service/proj.go
+++ service/proj.go
@@ -29,10 +29,10 @@ func UpdateProj(proj *model.Proj) (err error) {
 
 func UpdateProjStatus(proj *model.Proj) (err error) {
 	// 移入或移出回收站: 先改变 Prototype, Uml, Document 的 Status, 再保存 Proj
-	// 【注意】传入的 Proj 的 Status 已被修改
-	global.DB.Where("proj_id = ?", proj.ProjID).Updates(model.Prototype{Status: proj.Status})
-	global.DB.Where("proj_id = ?", proj.ProjID).Updates(model.Uml{Status: proj.Status})
-	global.DB.Where("proj_id = ?", proj.ProjID).Updates(model.Document{Status: proj.Status})
+	// 【注意】传入的 Proj 的 Status 应该是已被修改的
+	global.DB.Model(model.Prototype{}).Where("proj_id = ?", proj.ProjID).Updates(model.Prototype{Status: proj.Status})
+	global.DB.Model(model.Uml{}).Where("proj_id = ?", proj.ProjID).Updates(model.Uml{Status: proj.Status})
+	global.DB.Model(model.Document{}).Where("proj_id = ?", proj.ProjID).Updates(model.Document{Status: proj.Status})
 	err = global.DB.Save(proj).Error
 	return err
 }
@@ -70,7 +70,8 @@ func UpdatePrototype(prototype *model.Prototype) (err error) {
 	return err
 }
 
-func DeletePrototype(prototype *model.Prototype) (err error) { // 用于删除回收站中的原型, 如果移到回收站, 应使用 Update, 后同
+func DeletePrototype(prototype *model.Prototype) (err error) {
+	// 用于删除回收站中的原型, 如果移到回收站, 应使用 Update, 如果从回收站中恢复, 应先判断 Proj 是否在回收站中, 再使用 Update, 后同
 	err = global.DB.Delete(&prototype).Error
 	return err
 }
@@ -130,4 +131,58 @@ func UpdateDocument(document *model.Document) (err error) {
 func DeleteDocument(document *model.Document) (err error) {
 	err = global.DB.Delete(&document).Error
 	return err
+}
+
+func GetProjPrototypes(projID uint64, status int) (prototypes []model.Prototype, notFound bool) {
+	// status = 2 时, 查找回收站的项目, 后同
+	notFound = global.DB.Where("proj_id = ? and status = ?", projID, status).Find(&prototypes).RecordNotFound()
+	return prototypes, notFound
+}
+
+func GetProjUmls(projID uint64, status int) (umls []model.Uml, notFound bool) {
+	notFound = global.DB.Where("proj_id = ? and status = ?", projID, status).Find(&umls).RecordNotFound()
+	return umls, notFound
+}
+
+func GetProjDocuments(projID uint64, status int) (documents []model.Document, notFound bool) {
+	notFound = global.DB.Where("proj_id = ? and status = ?", projID, status).Find(&documents).RecordNotFound()
+	return documents, notFound
+}
+
+func GetUserProjs(userID uint64, status int, op int) (projs []model.Proj, notFound bool) {
+	// 查找该用户在所有团队中的项目
+	// op = 1 时查找"我创建的", op = 2 时查找"我参与的", op = 3 时查找"全部项目"
+	if op == 1 {
+		notFound = global.DB.Where("user_id = ? and status = ?", userID, status).Find(&projs).RecordNotFound()
+	} else if op == 2 {
+		notFound = global.DB.Raw("SELECT * FROM projs, identities "+
+			"WHERE projs.group_id = identities.group_id "+
+			"AND identities.user_id = ? AND projs.user_id != ? "+
+			"AND projs.status = ?;", userID, userID, status).Find(&projs).RecordNotFound()
+	} else {
+		notFound = global.DB.Raw("SELECT * FROM projs, identities "+
+			"WHERE projs.group_id = identities.group_id "+
+			"AND identities.user_id = ? AND projs.status = ?;", userID, status).Find(&projs).RecordNotFound()
+	}
+	return projs, notFound
+}
+
+func GetUserProjsInGroup(userID uint64, groupID uint64, status int, op int) (projs []model.Proj, notFound bool) {
+	// 查找该用户在某个团队中的项目
+	// op = 1 时查找"我创建的", op = 2 时查找"我参与的", op = 3 时查找"全部项目"
+	if op == 1 {
+		notFound = global.DB.Where("user_id = ? and group_id = ? and status = ?",
+			userID, groupID, status).Find(&projs).RecordNotFound()
+	} else if op == 2 {
+		notFound = global.DB.Raw("SELECT * FROM projs, identities "+
+			"WHERE identities.user_id = ? AND projs.user_id != ? "+
+			"AND projs.group_id = ? AND identities.group_id = ? "+
+			"AND projs.status = ?;", userID, userID, groupID, groupID, status).Find(&projs).RecordNotFound()
+	} else {
+		notFound = global.DB.Raw("SELECT * FROM projs, identities "+
+			"WHERE identities.user_id = ? "+
+			"AND projs.group_id = ? AND identities.group_id = ? "+
+			"AND projs.status = ?;", userID, groupID, groupID, status).Find(&projs).RecordNotFound()
+	}
+	return projs, notFound
 }
