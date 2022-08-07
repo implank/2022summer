1405a63492d49ebf6267e0270870ab57e97fed5f
wxy4869 <wangxinyuan4869@163.com>
2022-08-07 21:43:26 +0800

    fix&feat: 新增了时间的默认值, 新增了复制项目

--- model/database/proj.go
+++ model/database/proj.go
@@ -10,7 +10,7 @@ type Proj struct {
 	GroupID   uint64    `gorm:"not null;" json:"group_id"`        // 项目所属团队
 	UserID    uint64    `gorm:"not null;" json:"user_id"`         // 项目创建者
 	Top       int       `gorm:"not null;default:1;" json:"top"`   // 1 不置顶、2 置顶
-	CreatedAt time.Time `gorm:"not null" json:"created_at"`
-	UpdatedAt time.Time `gorm:"not null" json:"updated_at"`
+	CreatedAt time.Time `gorm:"not null;default:Now();" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null;default:Now();" json:"updated_at"`
 	EditTimes int       `gorm:"not null;default:0;" json:"edit_times"`
 }
